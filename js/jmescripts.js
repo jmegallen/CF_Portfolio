4fb02ec821a51862e11fc1349491072d916898e0
Jamie Gallen <jme@gmail.com>
2017-02-16 16:53:26 +0100

    Added JS file
    And switched all scripts into it

---
+++ js/jmescripts.js
@@ -0,0 +1,45 @@
+//TwitterButtonJS
+! function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0],
+		p = /^http:/.test(d.location) ? 'http' : 'https';
+	if (!d.getElementById(id)) {
+		js = d.createElement(s);
+		js.id = id;
+		js.src = p + '://platform.twitter.com/widgets.js';
+		fjs.parentNode.insertBefore(js, fjs);
+	}
+}(document, 'script', 'twitter-wjs');
+
+$(document).ready(function() {
+
+	// Tooltips
+	$(function() {
+		$('[data-toggle="tooltip"]').tooltip();
+	});  
+	$(function() {
+		$('#droptoolbut').tooltip();
+	});
+
+	// Stellar
+	$('#video').stellar();
+
+	// Smooth scrolling
+  var $root = $('html, body');
+  $('.navbar-nav a').click(function() {
+      var href = $.attr(this, 'href');
+      $root.animate({
+          scrollTop: $(href).offset().top
+      }, 500, function () {
+          window.location.hash = href;
+      });
+      return false;
+  });
+
+  //Event Listen
+  $('#subbut').on('click', function() {
+  // now we're going to work in here
+  var comment = $(".message-box").val();
+  return false;
+	});
+
+});
