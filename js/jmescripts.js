81c50d7cda209609512fd44f4ef385b02ca5ef03
Jamie Gallen <jme@gmail.com>
2017-02-22 16:07:40 +0100

    Facebook button & Hide Message Box js
    Facebook button & Hide Message Box js

--- js/jmescripts.js
+++ js/jmescripts.js
@@ -9,37 +9,35 @@
 		fjs.parentNode.insertBefore(js, fjs);
 	}
 }(document, 'script', 'twitter-wjs');
-
+//AfterDOM
 $(document).ready(function() {
-
 	// Tooltips
 	$(function() {
 		$('[data-toggle="tooltip"]').tooltip();
-	});  
+	});
 	$(function() {
 		$('#droptoolbut').tooltip();
 	});
-
 	// Stellar
 	$('#video').stellar();
-
 	// Smooth scrolling
-  var $root = $('html, body');
-  $('.navbar-nav a').click(function() {
-      var href = $.attr(this, 'href');
-      $root.animate({
-          scrollTop: $(href).offset().top
-      }, 500, function () {
-          window.location.hash = href;
-      });
-      return false;
-  });
-
-  //Event Listen
-  $('#subbut').on('click', function() {
-  // now we're going to work in here
-  var comment = $(".message-box").val();
-  return false;
+	var $root = $('html, body');
+	$('.navbar-nav a').click(function() {
+		var href = $.attr(this, 'href');
+		$root.animate({
+			scrollTop: $(href).offset().top
+		}, 500, function() {
+			window.location.hash = href;
+		});
+		return false;
 	});
-
-});
+	//Event Listen
+	$('#subbut').on('click', function() {
+		// now we're going to work in here
+		var comment = $('#message-box').val();
+		$('#visible-comment').html(comment);
+		$('#message-box').hide("slow");
+		console.log(comment);
+		return false;
+	});
+}); 
